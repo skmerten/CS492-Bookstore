6ea29b7c424c3007672f26210599636b102f30e9
Stephen Merten <skmerten@gmail.com>
2026-09-05 12:24:46 -0700

    Finished CRUD with search

--- Bookstore/settings.py
+++ Bookstore/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 LOGIN_URL = "/login/"
-LOGIN_REDIRECT_URL = "/inventory/manage/"
+LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
 # Application definition
--- inventory/urls.py
+++ inventory/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.books, name="books"),   
-    path("manage/", views.manage_inventory, name="manage_inventory"),
     path("manage/add/", views.add_book, name="add_book"),
     path("manage/edit/<int:book_id>/", views.edit_book, name="edit_book"),
     path("manage/delete/<int:book_id>/", views.delete_book, name="delete_book"),
